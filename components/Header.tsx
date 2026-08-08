1db90fb4d75d7f2bd838e5959437afa77dd93ba0
ChokY Juntak <liburan@choky.net>
2026-08-08 09:19:31 +0700

    menu di header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+  const isHomepage = pathname === "/";
+
   return (
     <header className="site-header">
       <div className="header-inner">
@@ -14,11 +20,17 @@ export default function Header() {
         <nav className="main-nav">
           <ul>
             <li><Link href="/">Home</Link></li>
-            <li><a href="#paket-wisata">Paket Wisata</a></li>
-            <li><Link href="/artikel">Artikel</Link></li>
-            <li><Link href="/galeri">Galeri</Link></li>
-            <li><Link href="/tentang-kami">Tentang Kami</Link></li>
-            <li><Link href="/faq">FAQ</Link></li>
+
+            {isHomepage && (
+              <>
+                <li><a href="#paket-wisata">Paket Wisata</a></li>
+                <li><Link href="/artikel">Artikel</Link></li>
+                <li><Link href="/galeri">Galeri</Link></li>
+                <li><Link href="/tentang-kami">Tentang Kami</Link></li>
+                <li><Link href="/faq">FAQ</Link></li>
+              </>
+            )}
+
             <li><a href="https://wa.me/+6282225336306" target="_blank" rel="noopener">Kontak</a></li>
           </ul>
         </nav>
